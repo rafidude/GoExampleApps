3e61cfed77e25bd2d5fccee536858b463e33fde4
Rafi Dudekula <rafi.dudekula@gmail.com>
2023-07-12 12:46:03 -0500

    more samples

---
+++ air1/go.mod
@@ -0,0 +1,3 @@
+module air1
+
+go 1.20
---
+++ air1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+// go install github.com/cosmtrek/air@latest
+// air -c .air.toml
+func main() {
+	fmt.Println("Hello World13!")
+}
